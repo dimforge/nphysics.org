40a26c27c32915820b2cbef00943505e561bc070
sebcrozet <developer@crozet.re>
2018-07-28 19:16:42 +0200

    Update helper scripts.

--- copy_doc.sh
+++ copy_doc.sh
@@ -4,7 +4,7 @@ out_dir=./docs/rustdoc
 nphysics_dir=../rust-dev/nphysics-dev
 
 echo "Generating the documentation..."
-cd $nphysics_dir; cargo doc --no-deps
+cd $nphysics_dir; cargo doc -p nphysics2d -p nphysics3d --no-deps
 cd -
 rm -rf docs/rustdoc
 cp -r $nphysics_dir/target/doc $out_dir
